a199a10e60ae6284c903059f01ab274871eedcdc
ycchang <yancheng.chang@gmail.com>
2022-07-20 20:42:12 +0800

    Change some wording

--- src/resources/i18n/en.ts
+++ src/resources/i18n/en.ts
@@ -7,7 +7,7 @@ const en = {
   'verify.text.noPdfError': 'Please import a .pdf file',
   'verify.text.noSpfError': 'Please import a .spf file',
   'verify.text.resultHint': 'The verification result will be showed in the next page.',
-  'verify.text.description1': 'To verify eSignatures identifiable by the Let\'s eSign totem (🀙🀛) in a PDF document, you would need to supply the corresponding signing proof (in the .spf file format). The documents you submit here won\'t leave this app.',
+  'verify.text.description1': 'To verify eSignatures identifiable by the Let\'s eSign totem (🀙🀛) in a PDF document, you would need to supply the corresponding signing proof (in the .spf file format). The documents you submit here won\'t leave your browser.',
   'verify.text.description2': 'The signing proof contains the so-called attestation document, which is a piece of evidence of how the identity and the intent of a signer are confirmed. It is created and digitally signed by specific AWS hardware.',
   'verify.text.submit': 'Submit',
   'verify.text.noPDFFileChosen': 'Need a .pdf file',
--- src/resources/i18n/zh-TW.ts
+++ src/resources/i18n/zh-TW.ts
@@ -7,7 +7,7 @@ const zhTW = {
   'verify.text.noPdfError': '請匯入.pdf檔案',
   'verify.text.noSpfError': '請匯入.spf檔案',
   'verify.text.resultHint': '驗證結果將會在下一頁秀出。',
-  'verify.text.description1': '若要驗證 PDF 文件中可透過 Let\'s eSign 圖騰 (🀙🀛) 來識別的電子簽名，您需提供相對應的簽名佐證 signig proof；簽名佐證的副檔名為.spf。您在此提交的文件並不會離開此應用程式。',
+  'verify.text.description1': '若要驗證 PDF 文件中可透過 Let\'s eSign 圖騰 (🀙🀛) 來識別的電子簽名，您需提供相對應的簽名佐證 signig proof；簽名佐證的副檔名為.spf。您在此提交的文件並不會離開您的瀏覽器。',
   'verify.text.description2': '簽名佐證內含所謂的 attestation document，一種由特定 AWS 硬體產生同時數位簽署的證據。它可以用來證明究竟簽署者的身分和簽名意願是如何被確認的。',
   'verify.text.submit': '提交',
   'verify.text.noPDFFileChosen': '需要一個 .pdf 檔案',
